be399cde3a8fe6f2299e6894ca731862cd7af7d7
Vo Duc Phuong <phuongvd201@gmail.com>
2019-04-04 00:50:12 +0700

    add

--- S3ImageProcessing/S3ImageProcessing.Services/Implementations/ParsedImageStore.cs
+++ S3ImageProcessing/S3ImageProcessing.Services/Implementations/ParsedImageStore.cs
@@ -38,7 +38,7 @@ namespace S3ImageProcessing.Services.Implementations
                 {
                     try
                     {
-                        for (int i = 0; i <= histograms.Length - 1; i++)
+                        for (int i = 0; i < histograms.Length; i++)
                         {
                             using (var command = _dbAccess.CreateCommand(
                                 sql,
