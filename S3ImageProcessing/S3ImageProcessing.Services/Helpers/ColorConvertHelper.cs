1948aa0adca15bc2bdd95c9178bec9363ac2ebf7
Vo Duc Phuong <phuongvd201@gmail.com>
2019-04-04 12:25:30 +0700

    reafactor

--- S3ImageProcessing/S3ImageProcessing.Services/Helpers/ColorConvertHelper.cs
+++ S3ImageProcessing/S3ImageProcessing.Services/Helpers/ColorConvertHelper.cs
@@ -4,9 +4,9 @@
     {
         public static byte To8BitColorByte(byte red, byte green, byte blue)
         {
-            var value8Bit = (byte)(((byte)(red / 32) << 5) + ((byte)(green / 32) << 2) + (byte)(blue / 64));
+            var color8bit = (byte)(((byte)(red / 32) << 5) + ((byte)(green / 32) << 2) + (byte)(blue / 64));
 
-            return value8Bit;
+            return color8bit;
         }
     }
 }
